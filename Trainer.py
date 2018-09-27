8f1ddc29540c8aa078c8a38a02effd56f3857d2d
Noah Dominic Miranda Silvio <ndsilvio@outlook.ph>
2018-09-27 14:50:49 +0800

    Change graph colours

--- Trainer.py
+++ Trainer.py
@@ -24,7 +24,24 @@ for i in range(50):
 # training
 p = Perceptron(2, 0.001)
 
-print(int(p.feed_forward([75, -43])))
+testing_data = open("testing_data.txt", "r")
+testing_data_str = testing_data.read().split()
+
+plots_x_testing = []
+plots_y_testing = []
+plots_class_testing = []
+
+for i in range(50):
+    plots_x_testing.append(int(testing_data_str[i*3]))
+    plots_y_testing.append(int(testing_data_str[i*3 + 1]))
+    if int(testing_data_str[i * 3 + 2]) == p.feed_forward([int(testing_data_str[i*3]), int(testing_data_str[i*3 + 1])]):
+        plots_class_testing.append('g')
+    else:
+        plots_class_testing.append('k')
+
+scatter(plots_x_testing, plots_y_testing, s=100, marker='.', c=plots_class_testing)
+
+show()
 
 loss_x = []
 loss_y = []
@@ -37,27 +54,35 @@ for i in range(5000):
     loss_x.append(i)
     loss_y.append(np.mean(training_loss))
 
+
+# show loss function
 scatter(loss_x, loss_y, s=100, marker='.', c='k')
 show()
+
 # show results
 
-testing_data = open("testing_data.txt", "r")
-testing_data_str = testing_data.read().split()
-
+# reset vectors
 plots_x_testing = []
 plots_y_testing = []
 plots_class_testing = []
+plots_class_correctness = []
 
 for i in range(50):
     plots_x_testing.append(int(testing_data_str[i*3]))
     plots_y_testing.append(int(testing_data_str[i*3 + 1]))
     if int(testing_data_str[i * 3 + 2]) == p.feed_forward([int(testing_data_str[i*3]), int(testing_data_str[i*3 + 1])]):
+        plots_class_correctness.append('g')
+    else:
+        plots_class_correctness.append('k')
+
+    if int(testing_data_str[i * 3 + 2]) == 1:
         plots_class_testing.append('r')
     else:
         plots_class_testing.append('b')
 
 scatter(plots_x_testing, plots_y_testing, s=100, marker='.', c=plots_class_testing)
-
+show()
+scatter(plots_x_testing, plots_y_testing, s=100, marker='.', c=plots_class_correctness)
 show()
 
 print(int(p.feed_forward([75, -43])))
